df082adf105296fe2654e63b645339e4614dbbb7
grzegorzpryjma99 <62578864+grzegorzpryjma99@users.noreply.github.com>
2023-08-20 15:02:31 +0200

    vercel analitics

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const handler = (req, res) => {
+  res.setHeader('Content-Security-Policy', "connect-src 'self' vitals.vercel-insights.com");
+};
+
+module.exports = handler;
 module.exports = nextConfig
